ffaa335f501bc1965f2fd8abda234cd12c071893
Robert Bagge <bagge.robert@gmail.com>
2015-04-13 14:47:53 +0200

    Added functionality so that Wifi-data is recorded periodically every 30 sec

--- app/src/main/java/com/locationlocationlocation/simulation/SensorService.java
+++ app/src/main/java/com/locationlocationlocation/simulation/SensorService.java
@@ -1,23 +1,54 @@
 package com.locationlocationlocation.simulation;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SensorService extends Service {
+    private static String TAG;
     private static String SERVICE_TAG = "Service";
+
+    private static String FOLDER_NAME_ACCELEROMETER = "accelerometer_data";
+    private static String FOLDER_NAME_WIFI = "wifi_data";
+
+    private final static String WIFI_DATA_FILE = "wifi_data.txt";
+    private final static String ACCELEROMETER_DATA_FILE = "accelerometer_data.txt";
+    OutputStreamWriter mOutputWifi;
+    OutputStreamWriter mOutputAccelerometer;
+
+    private String mFolderPathWifi;
+    private String mFolderPathAccelerometer;
+
+    private WifiManager mWifiManager;
+    private BroadcastReceiver mWifiReceiver;
+
     private SensorManager mSensorManager;
     private Sensor mAccelerometer, mMagnetometer;
     private SensorEventListener mAccelerometerListener, mMagnetometerListener;
@@ -25,19 +56,65 @@ public class SensorService extends Service {
     private FileWriter writer;
     private String currentActivityName;
     private StringBuffer sb;
+    private static int ACCELEROMETER_FREQUENCY = 100;
+    private Handler wHandler;
+    private Timer timer;
+    private TimerTask doAsynchronousTask;
+
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        int hz = intent.getIntExtra("hz", SensorManager.SENSOR_DELAY_FASTEST);
-        this.currentActivityName = intent.getStringExtra("currentactivity");
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        TAG = SensorService.this.getClass().getSimpleName();
+        Log.e(TAG, "onStartCommand()");
+        /*mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mAccelerometerListener = new AccelerometerEventListener();
         mMagnetometerListener = new MagnetometerEventListener();
-        mSensorManager.registerListener(mAccelerometerListener, mAccelerometer, hz);
-        mSensorManager.registerListener(mMagnetometerListener, mMagnetometer, hz);
-        this.sb = new StringBuffer();
+        mSensorManager.registerListener(mAccelerometerListener, mAccelerometer, this.ACCELEROMETER_FREQUENCY);
+        mSensorManager.registerListener(mMagnetometerListener, mMagnetometer, this.ACCELEROMETER_FREQUENCY);
+        this.sb = new StringBuffer();*/
+
+        String sourceFolderName = intent.getStringExtra("folder_name");
+        mFolderPathWifi = sourceFolderName + "/" + this.FOLDER_NAME_WIFI;
+        mFolderPathAccelerometer = sourceFolderName + "/" + this.FOLDER_NAME_ACCELEROMETER;
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        //mWifiReceiver = new WifiScanReceiver();
+
+
+        try{
+            File mWifiOutputFile = new File(sourceFolderName + "/" + WIFI_DATA_FILE);
+            FileOutputStream mWifiOutputStream = new FileOutputStream(mWifiOutputFile);
+            mOutputWifi = new OutputStreamWriter(mWifiOutputStream);
+            mOutputWifi.write("Run initiated" + System.lineSeparator());
+            Log.e(TAG, "Writing to mOutputWifi initiated");
+        }catch(FileNotFoundException e1){
+            e1.printStackTrace();
+            Log.e(TAG, "Aborted, can't save to file");
+        }catch(IOException e2){
+            e2.printStackTrace();
+        }
+
+        try{
+            File mAccelerometerOutputFile = new File(sourceFolderName + "/" + ACCELEROMETER_DATA_FILE);
+            FileOutputStream mAccelerometerOutputStream = new FileOutputStream(mAccelerometerOutputFile);
+            mOutputAccelerometer = new OutputStreamWriter(mAccelerometerOutputStream);
+            mOutputAccelerometer.write("Run initiated" + System.lineSeparator());
+            Log.e(TAG, "Writing to mOutputWifi initiated");
+        }catch(FileNotFoundException e1){
+            e1.printStackTrace();
+            Log.e(TAG, "Aborted, can't save to file");
+        }catch(IOException e2){
+            e2.printStackTrace();
+        }
+
+        folderExists(mFolderPathWifi);
+        folderExists(mFolderPathAccelerometer);
+
+        Toast.makeText(this, sourceFolderName, Toast.LENGTH_LONG).show();
+        AsyncTimer();
 
 
         return START_STICKY;
@@ -45,9 +122,82 @@ public class SensorService extends Service {
 
     @Override
     public void onDestroy(){
-        mSensorManager.unregisterListener(mAccelerometerListener);
-        mSensorManager.unregisterListener(mMagnetometerListener);
-        writeToFile();
+        Log.e(TAG, "onDestroy()");
+        timer.cancel();
+        //mSensorManager.unregisterListener(mAccelerometerListener);
+        //mSensorManager.unregisterListener(mMagnetometerListener);
+        //unregisterReceiver(mWifiReceiver);
+
+        //writeToFile();
+        try {
+            Log.e(TAG, "closing mOutputWifi");
+            mOutputWifi.write("Run Closed" + System.lineSeparator());
+            mOutputWifi.close();
+        } catch (IOException e) {
+            Log.e(TAG, "io exception when closing wifi output stream");
+            e.printStackTrace();
+        }
+    }
+
+    public void AsyncTimer() {
+        wHandler = new Handler();
+        timer = new Timer();
+        doAsynchronousTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                wHandler.post(new Runnable() {
+                    public void run() {
+                        try {
+
+                            long scanId = System.currentTimeMillis();
+                            Log.e(TAG, "Timer task running: " + Long.toString(scanId));
+
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy_dd_MM_HH_mm_ss");
+                            Calendar calendar = Calendar.getInstance();
+                            calendar.setTimeInMillis(scanId);
+                            String timestamp = sdf.format(calendar.getTime());
+
+                            WifiScan wifiScan = new WifiScan(mFolderPathWifi + "/" + timestamp + ".txt");
+                            wifiScan.execute();
+                        }catch (Exception e) {}
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 0, 30000); // Repeat in every 30000 sec
+    }
+
+    // Async task - wifi scan
+    public class WifiScan extends AsyncTask<Void, String, Void> {
+
+        String filePath;
+
+        public WifiScan(String filePath){
+            this.filePath = filePath;
+        }
+
+        protected void onPreExecute() {
+
+        }
+
+        protected void onPostExecute(Void results) {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Log.e(TAG, "Writing to wifi output stream new scan");
+                mOutputWifi.write("===========================" + System.lineSeparator());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            registerReceiver(new WifiScanReceiver(this.filePath), new IntentFilter(
+                    WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+            mWifiManager.startScan();
+            return null;
+        }
     }
 
     private void writeToFile() {
@@ -153,6 +303,67 @@ public class SensorService extends Service {
                         ", " + holder.value2 + ", " + holder.value3 + ", " + holder.type + "\n");
             }
             return null;
+        }
+    }
+
+    public class WifiScanReceiver extends BroadcastReceiver {
+        String filePath;
+
+        public WifiScanReceiver(String filePath){
+            super();
+            this.filePath = filePath;
+        }
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            List<ScanResult> wifiScanList = mWifiManager.getScanResults();
+            try {
+                mOutputWifi.write(filePath + System.lineSeparator());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(filePath, true);
+
+                List<String[]> data = new ArrayList<String[]>();
+                ScanResult row = null;
+                for(int i = 0; i < wifiScanList.size(); i++){
+                    row = wifiScanList.get(i);
+                    writer.write(row.BSSID + ", " + row.SSID + ", " + String.valueOf(convertFrequencyToChannel(row.frequency)) + ", " + String.valueOf(row.level) + System.lineSeparator());
+                    //data.add(new String[] {row.BSSID, row.SSID, String.valueOf(convertFrequencyToChannel(row.frequency)), String.valueOf(row.level)});
+                }
+                /*for (int i = 0; i < wifiScanList.size(); i++) {
+                    ScanResult sr1 = mWifiManager.getScanResults().get(i);
+                    System.out.println("BSSID: " + sr1.BSSID);
+                    System.out.println("RSSI: " + sr1.level);
+
+                    String str = "BSSID: " + sr1.BSSID + " " + "Level: " + sr1.level;
+
+                    try {
+                        Log.e(TAG, "Writing to mOutputWifi onReceive");
+                        mOutputWifi.write(str.toString() + System.lineSeparator());
+                        writer.write(str.toString());
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error when writing wifi scanResults to file");
+                        e.printStackTrace();
+                    }
+                }*/
+                //writer.writeAll(data);
+                writer.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            unregisterReceiver(this);
+        }
+    }
+
+    private static int convertFrequencyToChannel(int freq) {
+        if (freq >= 2412 && freq <= 2484) {
+            return (freq - 2412) / 5 + 1;
+        } else if (freq >= 5170 && freq <= 5825) {
+            return (freq - 5170) / 5 + 34;
+        } else {
+            return -1;
         }
     }
 }
--- app/src/main/java/com/locationlocationlocation/simulation/SimulationActivity.java
+++ app/src/main/java/com/locationlocationlocation/simulation/SimulationActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,74 +25,81 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 
-public class SimulationActivity extends Activity implements SensorEventListener {
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private Sensor mMagnetometer;
-    private float x,y,z;
-    float[] mAcceleration;
-    float[] mGeomagnetic;
-    private TextView acceleration;
-    private TextView orientationText;
-    private FileWriter writer;
-    private Button startButton;
+public class SimulationActivity extends Activity {
     private static String TAG = "Just det";
     private static String STARTED_RECORDING = "Started recording, press stop to stop the recording and save the results to a file";
     private static String STOPPED_RECORDING = "Stopped recording, the results has been saved to a file at: ";
     private static String FAULTY_SETTINGS = "You need to choose a sample rate and an activity";
-    private enum Activities {NONE, SITTING, STANDING, WALKING, CYCLING, GOING_BY_CAR, GOING_BY_BUS, GOING_BY_TRAIN}
-    private enum SamplingRates {NONE, Hz23, Hz46, Hz100, Hz200, FASTEST}
-    private Activities currentActivity;
-    private SamplingRates currentSamplingRate;
     private Context context;
-    private String filePath;
-    private long startTimeInMillis;
-    private AccelerometerEventListener mAccelerometerListener;
-    private MagnetometerEventListener  mMagnetometerListener;
-    private Intent intent;
+    private String appFolderPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accelerometer);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        acceleration = (TextView)findViewById(R.id.acceleration);
-        orientationText = (TextView)findViewById(R.id.orientation);
+        setContentView(R.layout.activity_simulation);
+        appFolderPath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/DeviceSimulation";
+        folderExists(appFolderPath);
 
-        Spinner spinner = (Spinner) findViewById(R.id.activity_spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        Spinner activitySpinner = (Spinner) findViewById(R.id.activity_spinner);
-
-        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(this, R.array.activities_array, android.R.layout.simple_spinner_item);
-
-        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        activitySpinner.setAdapter(activityAdapter);
-        activitySpinner.setOnItemSelectedListener(new ActivitiesOnItemSelectedListener());
-
-        Spinner sampleRateSpinner = (Spinner) findViewById(R.id.sample_rate_spinner);
-
-        ArrayAdapter<CharSequence> sampleRateAdapter = ArrayAdapter.createFromResource(this,
-                R.array.sample_rate_array, android.R.layout.simple_spinner_item);
-
-        sampleRateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        sampleRateSpinner.setAdapter(sampleRateAdapter);
-        sampleRateSpinner.setOnItemSelectedListener(new SamplingRatesOnItemSelectedListener());
-
-        currentActivity = Activities.NONE;
-        currentSamplingRate = SamplingRates.NONE;
         context = getApplicationContext();
 
     }
 
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        intent = new Intent(getApplicationContext(), SensorService.class);
+    public void startRecording(View view) {
+
+        EditText runName = (EditText)findViewById(R.id.run_name);
+        String runNameStr = runName.getText().toString();
+        File folder = new File(appFolderPath + "/" + runNameStr);
+        if(folder.exists() || runNameStr == ""){
+            Toast.makeText(this, "You need to type run name", Toast.LENGTH_SHORT).show();
+        }else{
+            ((Button)findViewById(R.id.stop_button)).setVisibility(View.VISIBLE);
+            view.setVisibility(View.GONE);
+            Toast.makeText(this, "startScan()", Toast.LENGTH_SHORT).show();
+            folder.mkdir();
+            startScan(folder.getAbsolutePath());
+            //Start service
+        }
+        //startScan();
+    }
+
+
+    public void stopRecording(View view) {
+        ((Button) findViewById(R.id.start_button)).setVisibility(View.VISIBLE);
+        view.setVisibility(View.GONE);
+        Toast.makeText(this, "stopScan()", Toast.LENGTH_SHORT).show();
+        stopScan();
+    }
+
+    public void startScan(String folderName){
+        Intent intent = new Intent(getApplicationContext(), SensorService.class );
+        intent.putExtra("folder_name", folderName);
+        startService(intent);
+
+    }
+
+    public void stopScan(){
+        Toast.makeText(this, "stopScan()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, SimulationActivity.STOPPED_RECORDING, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(getApplicationContext(), SensorService.class);
         stopService(intent);
+    }
+
+    public boolean validateRunName(String runNameStr){
+        File folder = new File(appFolderPath + "/" + runNameStr);
+        if(folder.exists() || runNameStr == ""){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private boolean folderExists(String folderPath){
+        File folder = new File(folderPath);
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdir();
+        }
+        return success;
     }
 
     @Override
@@ -114,208 +122,5 @@ public class SimulationActivity extends Activity implements SensorEventListener 
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startRecording(View view) {
-        if (validateUserInput()){
-            ((Button)findViewById(R.id.stop_button)).setVisibility(View.VISIBLE);
-            view.setVisibility(View.GONE);
-            Toast.makeText(this, SimulationActivity.STARTED_RECORDING, Toast.LENGTH_LONG).show();
-            startScan();
-
-        }else{
-            Toast.makeText(this, SimulationActivity.FAULTY_SETTINGS, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void stopRecording(View view) {
-        ((Button)findViewById(R.id.start_button)).setVisibility(View.VISIBLE);
-        view.setVisibility(View.GONE);
-        stopScan();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event){
-        //Log.d(TAG, "System.currentTimeInMillis(): " + Float.toString(System.currentTimeMillis()));
-
-        int sensorType = event.sensor.getType();
-        if (sensorType == Sensor.TYPE_ACCELEROMETER){
-            mAcceleration = event.values;
-            x = event.values[0];
-            y = event.values[1];
-            z = event.values[2];
-            //acceleration.setText("X: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2]);
-            //acceleration.setText("X: " + x + "\nY: " + y + "\nZ: " + z);
-            try {
-                writer.write(event.timestamp + ", " + event.values[0] + "," + event.values[1] + "," + event.values[2] + " " + sensorType + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (sensorType == Sensor.TYPE_MAGNETIC_FIELD)
-            mGeomagnetic = event.values;
-        if (mAcceleration != null && mGeomagnetic != null) {
-            float R[] = new float[9];
-            float I[] = new float[9];
-            boolean success = SensorManager.getRotationMatrix(R, I, mAcceleration, mGeomagnetic);
-            if (success) {
-                float orientation[] = new float[3];
-                SensorManager.getOrientation(R, orientation);// orientation contains: azimut, pitch and roll
-                //orientationText.setText("A: " + 57.2957795f * orientation[0] + "\nP: " + 57.2957795f * orientation[1] + "\nR: " + 57.2957795f * orientation[2]);
-
-                try {
-                    writer.write(event.timestamp + ", " + 57.2957795f * orientation[0] + ", " + 57.2957795f * orientation[1] + ", " + 57.2957795f * orientation[2] + " " + sensorType + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    private void startScan(){
-        intent = new Intent(getApplicationContext(), SensorService.class );
-        intent.putExtra("hz", hzToMys(currentSamplingRate.name()));
-        intent.putExtra("currentactivity", currentActivity.name());
-        startService(intent);
-    }
-
-    private int hzToMys(String hzString) {
-        if(hzString == SamplingRates.FASTEST.name())
-            return SensorManager.SENSOR_DELAY_FASTEST;
-
-        int hz = Integer.valueOf(hzString.substring(2));
-        return 1000000/hz;
-    }
-
-    private void stopScan(){
-        /*mSensorManager.unregisterListener(mAccelerometerListener);
-        mSensorManager.unregisterListener(mMagnetometerListener);
-        if(writer != null) {
-            try {
-                Toast.makeText(this, SimulationActivity.STOPPED_RECORDING + "\n" +  filePath, Toast.LENGTH_LONG).show();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        startTimeInMillis = 0;*/
-        Toast.makeText(this, SimulationActivity.STOPPED_RECORDING + "\n" +  filePath, Toast.LENGTH_LONG).show();
-        intent = new Intent(getApplicationContext(), SensorService.class);
-        stopService(intent);
-    }
-
-    private boolean validateUserInput(){
-        boolean validated = false;
-        if(currentActivity != Activities.NONE && currentSamplingRate != SamplingRates.NONE){
-            validated = true;
-        }
-
-        return validated;
-    }
-
-
-    private boolean folderExists(String folderPath){
-        File folder = new File(folderPath);
-        boolean success = true;
-        if (!folder.exists()) {
-            success = folder.mkdir();
-        }
-        return success;
-    }
-
-    private class ActivitiesOnItemSelectedListener implements AdapterView.OnItemSelectedListener{
-
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int pos, long id) {
-            // An item was selected. You can retrieve the selected item using
-            Log.d(TAG + "pos", Long.toString(parent.getItemIdAtPosition(pos)));
-            Log.d(TAG + "pos pos ", Integer.toString(pos));
-            //parent.getItemAtPosition(pos)
-            currentActivity = Activities.values()[pos];
-            Toast.makeText(context, currentActivity.name(), Toast.LENGTH_SHORT).show();
-        }
-
-        public void onNothingSelected(AdapterView<?> parent) {
-            // Another interface callback
-        }
-
-    }
-
-    private class SamplingRatesOnItemSelectedListener implements AdapterView.OnItemSelectedListener{
-
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int pos, long id) {
-            // An item was selected. You can retrieve the selected item using
-            Log.d(TAG + "pos", Long.toString(parent.getItemIdAtPosition(pos)));
-            Log.d(TAG + "pos pos ", Integer.toString(pos));
-            //parent.getItemAtPosition(pos)
-            currentSamplingRate = SamplingRates.values()[pos];
-            Toast.makeText(context, currentSamplingRate.name(), Toast.LENGTH_SHORT).show();
-        }
-
-        public void onNothingSelected(AdapterView<?> parent) {
-            // Another interface callback
-        }
-
-    }
-
-    private class AccelerometerEventListener implements SensorEventListener{
-        long previousEventTime = 0;
-        @Override
-        public void onSensorChanged(SensorEvent event){
-            if (previousEventTime == 0)
-                previousEventTime = event.timestamp;
-
-            mAcceleration = event.values;
-            try {
-                writer.write(event.timestamp - previousEventTime + ", " + event.values[0] + "," + event.values[1] + "," + event.values[2] + " " + event.sensor.getType() + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            previousEventTime = event.timestamp;
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    }
-
-    private class MagnetometerEventListener implements SensorEventListener{
-        long previousEventTime = 0;
-        @Override
-        public void onSensorChanged(SensorEvent event){
-            if (previousEventTime == 0)
-                previousEventTime = event.timestamp;
-            mGeomagnetic = event.values;
-            if (mAcceleration != null && mGeomagnetic != null) {
-                float R[] = new float[9];
-                float I[] = new float[9];
-                boolean success = SensorManager.getRotationMatrix(R, I, mAcceleration, mGeomagnetic);
-                if (success) {
-                    float orientation[] = new float[3];
-                    SensorManager.getOrientation(R, orientation);// orientation contains: azimut, pitch and roll
-                    try {
-                        writer.write(event.timestamp - previousEventTime + ", " + 57.2957795f * orientation[0] + ", " + 57.2957795f * orientation[1] + ", " + 57.2957795f * orientation[2] + " " + event.sensor.getType() + "\n");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            previousEventTime = event.timestamp;
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
     }
 }
